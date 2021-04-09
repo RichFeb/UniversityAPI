899e77d648c98636e814f4a9ab9d2113125beb9b
Shishikyuu <richard.febres@gmail.com>
2021-04-08 21:16:18 -0400

    Remove spaces

--- Startup.cs
+++ Startup.cs
@@ -3,10 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using NLog;
 using Microsoft.OpenApi.Models;
 using System.IO;
+using NLog;
 
 namespace UniversityAPI
 {
